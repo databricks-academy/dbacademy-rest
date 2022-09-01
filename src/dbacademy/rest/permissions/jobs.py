50f4e9cd5219ce6a932467632b4acb1284102b0d
Jacob D. Parr <jacob.parr@databricks.com>
2022-09-01 18:48:04 -0500

    Update jobs.py

--- src/dbacademy/rest/permissions/jobs.py
+++ src/dbacademy/rest/permissions/jobs.py
@@ -5,6 +5,7 @@ __all__ = ["Jobs"]
 
 
 class Jobs(PermissionsCrud):
+    valid_permissions = ["IS_OWNER", "CAN_MANAGE_RUN", "CAN_VIEW", "CAN_MANAGE"]
 
     def __init__(self, client: ApiClient):
         super().__init__(client, "2.0/permissions/jobs", "job")
