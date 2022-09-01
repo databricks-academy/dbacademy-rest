bcb01e5a1cbe3905db5143c06505647f39104fab
Jacob D. Parr <jacob.parr@databricks.com>
2022-09-01 17:21:44 -0500

    Update authorization_tokens.py

--- src/dbacademy/rest/permissions/authorization_tokens.py
+++ src/dbacademy/rest/permissions/authorization_tokens.py
@@ -1,8 +1,11 @@
+from typing import Any, Dict, List, Literal
 from dbacademy.rest.common import ApiClient
 from dbacademy.rest.permissions.crud import PermissionsCrud
 
 __all__ = ["Tokens"]
 
+# noinspection PyProtectedMember
+from dbacademy.rest.permissions.crud import What, PermissionLevel
 
 class Tokens(PermissionsCrud):
     valid_permissions = ["CAN_USE", "CAN_MANAGE"]
@@ -14,3 +17,14 @@ class Tokens(PermissionsCrud):
     def permission_levels(self):
         response = self.client.execute_get_json(f"{self.path}/permissionLevels")
         return response.get("permission_levels", [])
+
+    def update(self, object_id: str, what: What, value: str, permission_level: PermissionLevel):
+        self._validate_what(what)
+        self._validate_permission_level(permission_level)
+        acl = [
+                {
+                    what: value,
+                    "permission_level": permission_level
+                }
+            ]
+        return self.client.api_simple("PATCH", f"{self.path}", access_control_list=acl)
