e3621a7b743c933672bb59f3414d5ff14607f8b3
Jacob D. Parr <jacob.parr@databricks.com>
2022-09-01 19:05:45 -0500

    Update authorization_tokens.py

--- src/dbacademy/rest/permissions/authorization_tokens.py
+++ src/dbacademy/rest/permissions/authorization_tokens.py
@@ -1,30 +1,26 @@
-from typing import Any, Dict, List, Literal
-from dbacademy.rest.common import ApiClient
-from dbacademy.rest.permissions.crud import PermissionsCrud
+from dbacademy.dbrest import DBAcademyRestClient
+from dbacademy.rest.common import ApiContainer
 
 __all__ = ["Tokens"]
 
 # noinspection PyProtectedMember
 from dbacademy.rest.permissions.crud import What, PermissionLevel
 
-class Tokens(PermissionsCrud):
-    valid_permissions = ["CAN_USE", "CAN_MANAGE"]
+class Tokens(ApiContainer):
+    def __init__(self, client: DBAcademyRestClient):
+        self.client = client
+        self.base_url = f"{self.client.endpoint}/api/2.0/preview/tokens"
 
-    def __init__(self, client: ApiClient):
-        super().__init__(client, "2.0/preview/permissions/authorization/tokens", "token")
+    def get_levels(self) -> dict:
+        return self.client.execute_get_json(f"{self.base_url}/permissionLevels")
 
-    @property
-    def permission_levels(self):
-        response = self.client.execute_get_json(f"{self.path}/permissionLevels")
-        return response.get("permission_levels", [])
-
-    def update(self, object_id: str, what: What, value: str, permission_level: PermissionLevel):
-        self._validate_what(what)
-        self._validate_permission_level(permission_level)
-        acl = [
-                {
-                    what: value,
-                    "permission_level": permission_level
-                }
-            ]
-        return self.client.api_simple("PATCH", f"{self.path}", access_control_list=acl)
+    # def update(self, object_id: str, what: What, value: str, permission_level: PermissionLevel):
+    #     self._validate_what(what)
+    #     self._validate_permission_level(permission_level)
+    #     acl = [
+    #             {
+    #                 what: value,
+    #                 "permission_level": permission_level
+    #             }
+    #         ]
+    #     return self.client.api_simple("PATCH", f"{self.path}", access_control_list=acl)
