d285ed28c21185c21ff2d9743317d4507cf3a6e6
Jacob D. Parr <jacob.parr@databricks.com>
2022-09-07 11:24:54 -0500

    Changes from dev branch

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+#!python3
 import setuptools
 from setuptools import find_packages
 
@@ -7,16 +8,12 @@ reqs = [
     "urllib3",
     "overrides",
     "Deprecated",
-    #'dbacademy-gems@git+https://github.com/databricks-academy/dbacademy-gems',
 ]
 
 def find_dbacademy_packages():
     packages = find_packages(where="src")
     if "dbacademy" in packages:
         del packages[packages.index("dbacademy")]
-    # print("-"*80)
-    # print(packages)
-    # print("-"*80)
     return packages
 
 
